b7cd5fff76d241d4aa14ed64080060283cf08e09
VArmac <farrelnikoson@gmail.com>
2025-12-03 23:59:02 +0700

    VM branching

--- vm/vm.js
+++ vm/vm.js
@@ -1,9 +1,18 @@
+// const program1 = [
+//     ["const", 0, 10], // r0 => 10
+//     ["const", 1, 20], // r1 => 20
+//     ["add", 0, 1], // r0 = r0 + r1
+//     ["print", 0] // print r0
+// ]; // 01 0A 02 03
+
 const program = [
-    ["const", 0, 10], // r0 => 10
-    ["const", 1, 20], // r1 => 20
-    ["add", 0, 1], // r0 = r0 + r1
-    ["print", 0] // print r0
-]; // 01 0A 02 03
+    ["const", 0, 5], // r0 => 5
+    ["print", 0], // print
+    ["add", 0, -1], // r0--
+    ["jumpIfZero", 0, 5], // if r0 == 0 goto end
+    ["jump", 1], // jump to print
+    ["print", 0] // end
+];
 
 const vm = {
     regs: [0, 0, 0, 0], // 4 registers
@@ -18,8 +27,22 @@ while(vm.pc < program.length) {
         const [_, reg, val] = instr;
         vm.regs[reg] = val;
     } else if (op === "add") {
-        const [_, reg1, reg2] = instr;
-        vm.regs[reg1] += vm.regs[reg2];
+        const [_, reg, regIndex] = instr;
+        if (regIndex === -1) {
+            vm.regs[reg]--;
+        } else {
+            vm.regs[reg] += vm.regs[regIndex];
+        }
+    } else if (op === "jump") {
+        const [_, targetIndex] = instr;
+        vm.pc = targetIndex;
+        continue;
+    } else if (op === "jumpIfZero") {
+        const [_, reg, targetIndex] = instr;
+        if (vm.regs[reg] === 0) {
+            vm.pc = targetIndex;
+            continue;
+        }
     } else if (op === "print") {
         const [_, reg] = instr;
         const val = vm.regs[reg];

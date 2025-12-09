1b550d2127a2728ab6041a5bc136244b4d24d7ae
VArmac <farrelnikoson@gmail.com>
2025-12-09 23:46:10 +0700

    asm func and reboot syscall

--- misc/hello.s
+++ misc/hello.s
@@ -2,6 +2,15 @@
 .align 2
 
 _main:
-	mov X0, #69
-	mov X16, #1
-	svc 0
+    b _reboot
+    b _terminate
+
+_reboot:
+    mov X0, #1
+    mov X16, #55
+    svc 0
+
+_terminate:  
+    mov X0, #69
+    mov X16, #1
+    svc 0

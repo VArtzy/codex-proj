d0ce5535940049170db288f826c686c02110260c
VArmac <farrelnikoson@gmail.com>
2025-12-09 23:54:38 +0700

    hello world and write syscall in asm

--- misc/hello.s
+++ misc/hello.s
@@ -2,8 +2,15 @@
 .align 2
 
 _main:
-    b _reboot
+    b _printf
     b _terminate
+
+_printf:
+    mov X0, #1
+    adr X1, helloworld
+    mov X2, #13
+    mov X16, #4
+    svc 0
 
 _reboot:
     mov X0, #1
@@ -14,3 +21,5 @@ _terminate:
     mov X0, #69
     mov X16, #1
     svc 0
+
+helloworld: .ascii "Hello World\n"
